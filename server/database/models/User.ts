8bc0464d5126c06b6a888c1363755f0a7c31193f
Marshall Burns <marshallbcodes@gmail.com>
2023-05-23 07:20:02 -0500

    Update user entity

--- server/database/models/User.ts
+++ server/database/models/User.ts
@@ -1,12 +1,28 @@
 import { ModelUUID, type ModelUUIDData } from '@michealpearce/typeorm-models'
-import { Column, Entity } from 'typeorm'
+import { Column, CreateDateColumn, Entity } from 'typeorm'
 
 export interface UserData extends ModelUUIDData {
-	name: string
+	firstName: string
+	LastName: string
+	password: string
+	email: string
+	accountCreationDate: Date
 }
 
 @Entity()
 export class User extends ModelUUID<UserData> implements UserData {
 	@Column('varchar')
-	declare name: string
+	declare firstName: string
+
+	@Column('varchar')
+	declare LastName: string
+
+	@Column('varchar')
+	declare password: string
+
+	@Column({ unique: true })
+	declare email: string
+
+	@CreateDateColumn()
+	declare accountCreationDate: Date
 }
